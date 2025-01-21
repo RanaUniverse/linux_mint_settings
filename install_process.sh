4b400db2e6e8607caff7cfe058e095b23d60c9db
Rana Universe <ranauniverse321@gmail.com>
2025-01-21 12:58:17 +0530

    Update install_process.sh

--- install_process.sh
+++ install_process.sh
@@ -2,3 +2,6 @@
 
 echo "This is the installaitoni main file to get my settings and so on."
 echo "This has not made yet completely."
+
+
+i need to make a script which will open each folder and install these.
